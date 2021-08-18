9256524af560e999a490684fb8f92fcc64f1fac2
Botijo <jose_antoniocon@hotmail.com>
2021-08-18 09:46:51 +0200

    Login with FireBaseAuth OK

--- app/src/main/java/com/jacc/subastas/MainActivity.java
+++ app/src/main/java/com/jacc/subastas/MainActivity.java
@@ -1,12 +1,18 @@
 package com.jacc.subastas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -16,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText mEditTextEmail;
     private EditText mEditTextPass;
     private Button mButtonLogin;
+
+    private String email = "";
+    private String password = "";
 
     FirebaseAuth mAuth;
     DatabaseReference mDataBase;
@@ -35,13 +44,41 @@ public class MainActivity extends AppCompatActivity {
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                email = mEditTextEmail.getText().toString();
+                password = mEditTextPass.getText().toString();
 
+                if(!email.isEmpty() && !password.isEmpty()){
+                    loginUser();
+                }
+                else{
+                    Toast.makeText( MainActivity.this, "Completa los campos", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
 
     private void loginUser(){
-        mAuth.signInWithCredential()
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "No se pudo iniciar sesion, compruebas datos inicio", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null){
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            finish();
+        }
     }
 }
--- app/src/main/java/com/jacc/subastas/ProfileActivity.java
+++ app/src/main/java/com/jacc/subastas/ProfileActivity.java
@@ -1,14 +1,72 @@
 package com.jacc.subastas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button mButtonSignOut;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+    private TextView mTextViewEmail;
+    private TextView mTextViewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mButtonSignOut = (Button) findViewById(R.id.btnSiginout);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mTextViewEmail = (TextView) findViewById(R.id.textViewEmail);
+        mTextViewName = (TextView) findViewById(R.id.textViewName);
+
+        mButtonSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent( ProfileActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        getUserInfo();
+    }
+
+    private void getUserInfo(){
+        String id = mAuth.getCurrentUser().getUid();
+        mDatabase.child("user").child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String nombre = dataSnapshot.child("name").getValue().toString();
+                    String email = dataSnapshot.child("mail").getValue().toString();
+
+                    mTextViewName.setText(nombre);
+                    mTextViewEmail.setText(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
